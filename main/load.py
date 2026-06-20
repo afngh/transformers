0c9da0ac3c3a67ac76baeecedc1378674ffddc7e
afnan <147189348+afngh@users.noreply.github.com>
2026-06-20 07:29:57 +0000

    refactor code for checkpoints and pre-train

--- main/load.py
+++ main/load.py
@@ -1,6 +1,12 @@
-import pickle
+from main.generator_config._load_config_and_model import PretrainedHandler
 
-with open('config.pkl', 'rb') as f:
-    config_data = pickle.load(f)
+model_path = 'bin/model/model.pt'
+config_path = 'bin/data/config.pkl'
 
-print(config_data.locales)
+handler = PretrainedHandler(model_path, config_path)
+
+model, config = handler.load()
+
+client = handler.client(model, config)
+
+print(client.generate_response("warsaw is a city in poland and"))
