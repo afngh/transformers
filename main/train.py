1bef29d1a2a2d67720565b490b16e22a7526312c
Shaik Afnan <shaikafnan095@gmail.com>
2026-06-25 22:47:16 +0530

    Remove unused PATH variable and add configurable --checkpoint_path to train.py

--- main/train.py
+++ main/train.py
@@ -1,16 +1,15 @@
 import argparse
 from .fine_tune._fine_tune_model import FineTuneModel
 
-PATH = 'bin/model/model.pt'
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Train the Dynamo transformer model on BPE chunks.")
     parser.add_argument("file_path", type=str, help="Path to pre-tokenized training corpus (.pt file)")
     parser.add_argument("-s", "--start_chunk", type=int, default=None, help="Chunk index to start/resume training from")
     parser.add_argument("-v", "--val_path", type=str, default="bin/data/simple_wiki_val.pt", help="Path to pre-tokenized validation file (.pt file)")
+    parser.add_argument("-c", "--checkpoint_path", type=str, default="bin/model/model.pt", help="Path to model checkpoint (.pt file)")
 
     args = parser.parse_args()
 
-    model_loader = FineTuneModel(checkpoint_path=PATH)
+    model_loader = FineTuneModel(checkpoint_path=args.checkpoint_path)
     model_loader.train(file_path=args.file_path, start_chunk=args.start_chunk, val_path=args.val_path)
     model_loader.save()
